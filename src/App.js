ccad8840df07ea751706caeef3c241bd047fe773
David Heim <davidheim@bluewin.ch>
2017-05-11 13:50:28 +0200

    pizza counter component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React, {Component} from 'react';
-import PizzaSelection from './pizza-selection/PizzaSelection';
-import PizzaPreview from './pizza-preview/PizzaPreview';
+import PizzaCounter from './pizza-counter/PizzaCounter';
 
 class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedIngredients: []
-        };
-    }
-
     render() {
         return (
-            <main>
-                <PizzaSelection
-                    selectedPizza={this.state.selectedPizza}
-                    selectedIngredients={this.state.selectedIngredients}
-                    onSelectPizza={(selectedPizza) => this.setState({ selectedPizza })}
-                    onChangeIngredients={(selectedIngredients) => this.setState({ selectedIngredients })}
-                />
-                <PizzaPreview
-                    selectedPizza={this.state.selectedPizza}
-                    selectedIngredients={this.state.selectedIngredients}
-                />
-            </main>
+            <PizzaCounter />
         );
     }
 }
