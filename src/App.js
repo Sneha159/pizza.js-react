93a7c5547407915d132388d695e9b14b941b7be1
David Heim <davidheim@bluewin.ch>
2017-05-11 16:08:13 +0200

    refactor to stateless function component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import PizzaCounter from './pizza-counter/PizzaCounter';
 import PizzaCheckout from './pizza-checkout/PizzaCheckout';
-import { createStore } from 'redux';
-import { Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import pizzaApp from './pizzaApp.reducers';
 
 const store = createStore(pizzaApp);
 
-class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <div>
-                        <Route exact path="/" component={PizzaCounter}/>
-                        <Route path="/checkout" component={PizzaCheckout}/>
-                    </div>
-                </BrowserRouter>
-            </Provider>
-        );
-    }
-}
-
-export default App;
+export default () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={PizzaCounter}/>
+                <Route path="/checkout" component={PizzaCheckout}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+);
