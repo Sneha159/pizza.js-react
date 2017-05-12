3e13d3fa0cb7c7c8a63224aa9dae0e2b2d8a01d0
David Heim <davidheim@bluewin.ch>
2017-05-12 10:52:14 +0200

    input component, validation

--- src/pizza-checkout/AddressForm.js
+++ src/pizza-checkout/AddressForm.js
@@ -1,58 +1,72 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import PizzaInputField from './PizzaInputField';
+
+const requiredValidation = errorText =>
+    value => !value ? errorText : undefined;
 
 const AddressForm = ({ handleSubmit }) => (
     <form id="addressForm" onSubmit={handleSubmit}>
-        <div id="salutation" className="address-form__item address-form__item--full-width">
-            <select>
+        <div className="address-form__item address-form__item--full-width">
+            <Field
+                name="salutation"
+                component="select"
+            >
                 <option>Frau</option>
                 <option>Herr</option>
                 <option>Other</option>
-            </select>
+            </Field>
         </div>
 
-        <div id="firstName" className="address-form__item">
-            <Field
-                name="firstName"
-                type="text"
-                placeholder="Vorname"
-                component="input"
-                className="address-form__input"
-            />
-        </div>
+        <Field
+            name="firstName"
+            type="text"
+            placeholder="Vorname"
+            component={PizzaInputField}
+            validate={[requiredValidation('Vorname fehlt')]}
+        />
 
-        <div id="lastName" className="address-form__item">
-            <Field
-                name="lastName"
-                type="text"
-                placeholder="Nachname"
-                component="input"
-                className="address-form__input"
-            />
-            <span className="address-form__validation-error-text">Nachname fehlt</span>
-        </div>
+        <Field
+            name="lastName"
+            type="text"
+            placeholder="Nachname"
+            component={PizzaInputField}
+            validate={[requiredValidation('Nachname fehlt')]}
+        />
 
-        <div id="streetAndNumber" className="address-form__item address-form__item--full-width">
-            <Field
-                name="streetAndNumber"
-                type="text"
-                placeholder="Strasse / Nr"
-                component="input"
-                className="address-form__input"
-            />
-        </div>
+        <Field
+            name="streetAndNumber"
+            type="text"
+            placeholder="Strasse / Nr"
+            fullWidth
+            component={PizzaInputField}
+            validate={[requiredValidation('Adresse fehlt')]}
+        />
 
-        <div id="cityZip" className="address-form__item">
-            <input type="text" placeholder="PLZ" className="address-form__input"/>
-        </div>
+        <Field
+            name="cityZip"
+            type="number"
+            placeholder="PLZ"
+            component={PizzaInputField}
+            validate={[(value) => value >= 1000 && value <= 9999 ? undefined : 'PLZ ungültig']}
+        />
 
-        <div className="address-form__item">
-            <input type="text" placeholder="Ort" className="address-form__input"/>
-        </div>
+        <Field
+            name="city"
+            type="text"
+            placeholder="Ort"
+            component={PizzaInputField}
+            validate={[requiredValidation('Ort fehlt')]}
+        />
 
-        <div id="phoneNumber" className="address-form__item address-form__item--full-width">
-            <input type="tel" placeholder="Telefonnummer" className="address-form__input"/>
-        </div>
+        <Field
+            name="phoneNumber"
+            type="tel"
+            placeholder="Telefonnummer"
+            fullWidth
+            component={PizzaInputField}
+            validate={[requiredValidation('Telefonnummer fehlt')]}
+        />
 
         <div className="address-form__item address-form-item--align-right">
             <button type="submit" className="address-form__order-button">Bestellen!</button>
