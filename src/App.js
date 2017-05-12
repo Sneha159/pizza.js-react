ea5a5542469e86db7a38195e602614577efee956
David Heim <davidheim@bluewin.ch>
2017-05-12 09:04:22 +0200

    code formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PizzaCounter from './pizza-counter/PizzaCounter';
 import PizzaCheckout from './pizza-checkout/PizzaCheckout';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import pizzaApp from './pizzaApp.reducers';
 
 const store = createStore(pizzaApp);
