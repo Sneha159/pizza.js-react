e2c15541da8a7837c0545acfe9e0dc1ca768f466
r3dDoX <patrick@walther-online.ch>
2017-06-13 13:30:25 +0200

    extract more components

--- src/App.js
+++ src/App.js
@@ -1,81 +1,13 @@
 import React, { Component } from 'react';
 import PizzaPreview from './PizzaPreview';
+import PizzaSelection from './PizzaSelection';
 
 class App extends Component {
 
   render() {
     return (
       <main>
-        <div id="pizzaSelection">
-          <div className="pizza-tiles">
-            <div className="pizza-tile">
-              <div className="pizza-tile__avatar">
-                M
-              </div>
-              <div className="pizza-tile__content">
-                <h3 className="pizza-tile__title">Margherita</h3>
-                <p className="pizza-tile__description">
-                  Tomate, Mozzarella, Oregano
-                </p>
-              </div>
-            </div>
-            <div className="pizza-tile">
-              <div className="pizza-tile__avatar">
-                P&F
-              </div>
-              <div className="pizza-tile__content">
-                <h3 className="pizza-tile__title">Prosciutto et Funghi</h3>
-                <p className="pizza-tile__description">
-                  Tomate, Mozzarella, Oregano, Schinken, Pilze und noch mehr Pilze
-                </p>
-              </div>
-            </div>
-            <div className="pizza-tile">
-              <div className="pizza-tile__avatar">
-                M
-              </div>
-              <div className="pizza-tile__content">
-                <h3 className="pizza-tile__title">Margherita</h3>
-                <p className="pizza-tile__description">
-                  Tomate, Mozzarella, Oregano
-                </p>
-              </div>
-            </div>
-          </div>
-          <hr className="pizza-selection-divider" />
-          <div id="pizzaIngredients">
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Salami</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Schinken</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Artischocken</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Ananas</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Kapern</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Sardellen</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Büffelmozzarella</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Cherry-Tomaten</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Gruyère</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Rucola</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Speck</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Oliven</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Trüffel</label>
-            <label className="pizza-ingredients__item"><input type="checkbox" />
-              Scharf</label>
-          </div>
-          <hr className="pizza-selection-divider" />
-          <button className="order-button">Add to Shopping Cart</button>
-        </div>
+        <PizzaSelection />
         <PizzaPreview />
       </main>
     );
--- src/PizzaPreview.js
+++ src/PizzaPreview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ShoppingCart from './ShoppingCart';
 
 function togglePreview() {
   document.getElementById('pizzaPreview').classList.toggle('pizza-preview--open');
@@ -11,61 +12,7 @@ export default () => (
       <div id="pizzaAnimation">
         <img className="pizza-picture" src="/images/pizza.png" alt="Pizza" />
       </div>
-      <div id="shoppingCart">
-        <ul className="shopping-cart-list">
-          <li className="shopping-cart-list__item">
-            <div className="shopping-cart-list-item-title">
-              Margherita
-            </div>
-            <div className="shopping-cart-list-item-tools">
-              <select defaultValue={1}>
-                <option>0</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-              14.90
-            </div>
-          </li>
-          <li className="shopping-cart-list__item">
-            <div className="shopping-cart-list-item-title">
-              Calzone
-            </div>
-            <div className="shopping-cart-list-item-tools">
-              <select defaultValue={2}>
-                <option>0</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-              12.50
-            </div>
-          </li>
-          <li className="shopping-cart-list__item">
-            <div className="shopping-cart-list-item-title">
-              Prosciutto
-            </div>
-            <div className="shopping-cart-list-item-tools">
-              <select defaultValue={3}>
-                <option>0</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-              7.00
-            </div>
-          </li>
-        </ul>
-        <div className="shopping-cart-order">
-          <button className="order-button">Order</button>
-        </div>
-      </div>
+      <ShoppingCart />
     </div>
   </div>
 );
