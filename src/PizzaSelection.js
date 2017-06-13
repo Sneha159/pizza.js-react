33a03133540c04a750c353da0251c06256992456
r3dDoX <patrick@walther-online.ch>
2017-06-13 14:12:26 +0200

    use mock data for displaying pizzas

--- src/PizzaSelection.js
+++ src/PizzaSelection.js
@@ -1,74 +1,71 @@
 import React from 'react';
+import backend from './backend.json';
 
-export default () => (
-  <div id="pizzaSelection">
-    <div className="pizza-tiles">
-      <div className="pizza-tile">
-        <div className="pizza-tile__avatar">
-          M
+export default class PizzaSelection extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pizzas: backend.pizzas,
+    };
+  }
+
+  render() {
+    return (
+      <div id="pizzaSelection">
+        <div className="pizza-tiles">
+          {this.state.pizzas.map(pizza => (
+            <div
+              className="pizza-tile"
+              key={pizza.name}
+            >
+              <div className="pizza-tile__avatar">
+                {pizza.name.split(' ').map(name => name[0]).join('')}
+              </div>
+              <div className="pizza-tile__content">
+                <h3 className="pizza-tile__title">{pizza.name}</h3>
+                <p className="pizza-tile__description">
+                  {pizza.ingredients.join(', ')}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="pizza-tile__content">
-          <h3 className="pizza-tile__title">Margherita</h3>
-          <p className="pizza-tile__description">
-            Tomate, Mozzarella, Oregano
-          </p>
+        <hr className="pizza-selection-divider" />
+        <div id="pizzaIngredients">
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Salami</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Schinken</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Artischocken</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Ananas</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Kapern</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Sardellen</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Büffelmozzarella</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Cherry-Tomaten</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Gruyère</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Rucola</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Speck</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Oliven</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Trüffel</label>
+          <label className="pizza-ingredients__item"><input type="checkbox" />
+            Scharf</label>
         </div>
+        <hr className="pizza-selection-divider" />
+        <button className="order-button">Add to Shopping Cart</button>
       </div>
-      <div className="pizza-tile">
-        <div className="pizza-tile__avatar">
-          P&F
-        </div>
-        <div className="pizza-tile__content">
-          <h3 className="pizza-tile__title">Prosciutto et Funghi</h3>
-          <p className="pizza-tile__description">
-            Tomate, Mozzarella, Oregano, Schinken, Pilze und noch mehr Pilze
-          </p>
-        </div>
-      </div>
-      <div className="pizza-tile">
-        <div className="pizza-tile__avatar">
-          M
-        </div>
-        <div className="pizza-tile__content">
-          <h3 className="pizza-tile__title">Margherita</h3>
-          <p className="pizza-tile__description">
-            Tomate, Mozzarella, Oregano
-          </p>
-        </div>
-      </div>
-    </div>
-    <hr className="pizza-selection-divider" />
-    <div id="pizzaIngredients">
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Salami</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Schinken</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Artischocken</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Ananas</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Kapern</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Sardellen</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Büffelmozzarella</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Cherry-Tomaten</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Gruyère</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Rucola</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Speck</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Oliven</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Trüffel</label>
-      <label className="pizza-ingredients__item"><input type="checkbox" />
-        Scharf</label>
-    </div>
-    <hr className="pizza-selection-divider" />
-    <button className="order-button">Add to Shopping Cart</button>
-  </div>
-);
+    );
+  }
+}
